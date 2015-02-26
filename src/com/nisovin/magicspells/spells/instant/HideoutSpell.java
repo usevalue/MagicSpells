215c8432d467d5316e3d6a71273d6c3e7c58052b
usevalue <jurassic_pollard@hotmail.com>
2015-02-26 14:11:03 -0500

    Uploading Hideout/Retreat WITH ACTUAL CODE.

--- src/com/nisovin/magicspells/spells/instant/HideoutSpell.java
+++ src/com/nisovin/magicspells/spells/instant/HideoutSpell.java
@@ -1,7 +1,81 @@
 package com.nisovin.magicspells.spells.instant;
 
-/**
- * Created by james on 25/02/15.
- */
-public class HideoutSpell {
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.spells.InstantSpell;
+import com.nisovin.magicspells.util.MagicConfig;
+import com.nisovin.magicspells.util.MagicLocation;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.io.*;
+
+public class HideoutSpell extends InstantSpell {
+
+    private MagicLocation hideout;
+
+    public HideoutSpell(MagicConfig config, String spellName) {
+        super(config, spellName);
+        loadHideout();
+    }
+
+    @Override
+    public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
+        if (state == SpellCastState.NORMAL) {
+            Location loc = player.getLocation();
+            loc.setPitch(0);
+            hideout = new MagicLocation(loc);
+            saveHideout();
+        }
+        return PostCastAction.ALREADY_HANDLED;
+    }
+
+    public MagicLocation getHideout() {
+        return hideout;
+    }
+
+    private void loadHideout() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(MagicSpells.plugin.getDataFolder(), "hideouts/"+ internalName +".txt")));
+            try {
+                String line = reader.readLine();
+                if(line!="") {
+                    MagicSpells.error(line);
+                    String[] coords = line.split(",");
+                    World world = MagicSpells.plugin.getServer().getWorld(coords[0]);
+                    Double x = Double.parseDouble(coords[1]);
+                    Double y = Double.parseDouble(coords[2]);
+                    Double z = Double.parseDouble(coords[3]);
+                    Float yaw = Float.parseFloat(coords[4]);
+                    Location loc = new Location(world,x,y,z);
+                    loc.setYaw(yaw);
+                    loc.setPitch(0);
+                    hideout = new MagicLocation(loc);
+                }
+                else saveHideout();
+            } catch (Exception e) {
+                MagicSpells.error("Failed to load hideout for "+internalName+" spell.");
+                MagicSpells.error(e.toString());
+            }
+        }
+        catch (Exception e) {
+        }
+    }
+
+    private void saveHideout() {
+        try {
+            File hideoutDirectory = new File(MagicSpells.plugin.getDataFolder(),"hideouts");
+            if(!hideoutDirectory.exists()) hideoutDirectory.mkdir();
+            File file = new File(hideoutDirectory,internalName+".txt");
+            file.createNewFile();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file,false));
+            writer.write(hideout.getWorld() + "," + hideout.getX() + "," + hideout.getY() + "," + hideout.getZ() + "," + hideout.getYaw());
+            writer.close();
+        }
+        catch (Exception e) {
+            MagicSpells.error("Unable to save hideout for "+internalName+" spell.");
+            MagicSpells.error(e.toString());
+        }
+    }
+
 }
--- src/com/nisovin/magicspells/spells/instant/RetreatSpell.java
+++ src/com/nisovin/magicspells/spells/instant/RetreatSpell.java
@@ -1,7 +1,54 @@
 package com.nisovin.magicspells.spells.instant;
 
-/**
- * Created by james on 25/02/15.
- */
-public class RetreatSpell {
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.Spell;
+import com.nisovin.magicspells.spelleffects.EffectPosition;
+import com.nisovin.magicspells.spells.InstantSpell;
+import com.nisovin.magicspells.util.MagicConfig;
+import com.nisovin.magicspells.util.MagicLocation;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class RetreatSpell extends InstantSpell {
+
+    private String hideoutSpellName;
+    private String strNoHideout;
+    private String strRetreatFailed;
+
+    public RetreatSpell(MagicConfig config, String spellName) {
+        super(config, spellName);
+        hideoutSpellName = getConfigString("hideout-spell", "hideout");
+        strNoHideout = getConfigString("str-no-hideout", "No hideout has been set.");
+        strRetreatFailed = getConfigString("str-retreat-failed", "You were unable to retreat to the hideout.");
+    }
+
+    @Override
+    public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
+        if(state==SpellCastState.NORMAL) {
+            MagicLocation hideout = getHideout();
+            if(hideout==null) {
+                sendMessage(player, strNoHideout);
+                return PostCastAction.ALREADY_HANDLED;
+            }
+            Location from = player.getLocation();
+            Location destination = hideout.getLocation();
+            boolean tele = player.teleport(destination);
+            if(!tele) {
+                sendMessage(player, strRetreatFailed);
+            }
+            else {
+                playSpellEffects(EffectPosition.CASTER,from);
+                playSpellEffects(EffectPosition.TARGET,destination);
+            }
+        }
+        return PostCastAction.HANDLE_NORMALLY;
+    }
+
+    private MagicLocation getHideout() {
+        Spell spell = MagicSpells.getSpellByInternalName(hideoutSpellName);
+        if(spell!=null&&spell instanceof HideoutSpell) {
+            return ((HideoutSpell) spell).getHideout();
+        }
+        else return null;
+    }
 }
